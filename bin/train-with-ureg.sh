3d094735bc7aec2cc36caa4b1b0c8635a9b60d75
Fabien Campagne <fac2003@campagnelab.org>
2017-11-18 13:45:30 -0500

    Add train with ureg script.

---
+++ bin/train-with-ureg.sh
@@ -0,0 +1,5 @@
+# Run evaluate with a main model learning rate set to 0.1 and ureg alpha=0.1.
+
+python src/org/campagnelab/dl/pytorch/cifar10/Evaluate.py \
+-n 1000 --mini-batch-size 128 --ureg --checkpoint-key DEBUG \
+--lr 0.1 --ureg-learning-rate 0.1 --ureg-alpha 0.1 --ureg-num-features 64

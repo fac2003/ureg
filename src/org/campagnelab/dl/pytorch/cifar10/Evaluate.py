Fix ureg gradient flow.

src/org/campagnelab/dl/pytorch/cifar10/Evaluate.py
@@ -37,7 +37,7 @@ import torchvision.transforms as transforms
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 from org.campagnelab.dl.pytorch.cifar10.models import *
-from org.campagnelab.dl.pytorch.ureg import URegularizer
+from org.campagnelab.dl.pytorch.ureg. URegularizer import URegularizer
 from org.campagnelab.dl.pytorch.cifar10.utils import progress_bar
 
 parser = argparse.ArgumentParser(description='Evaluate ureg against CIFAR10')

src/org/campagnelab/dl/pytorch/ureg/URegularizer.py
@@ -1,8 +1,6 @@
 import torch
 from torch.autograd import Variable
 from torch.nn import Sequential
-from torch import cat
-from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 from org.campagnelab.dl.pytorch.cifar10.utils import init_params
 from org.campagnelab.dl.pytorch.ureg.MyFeatureExtractor import MyFeatureExtractor
@@ -239,19 +237,22 @@ class URegularizer:
         # the more the activations on the supervised set deviate from the unsupervised data,
         # the more we need to regularize:
         ys = self._which_one_model(supervised_output)
-        yu = self._which_one_model(unsupervised_output)
+        #yu = self._which_one_model(unsupervised_output)
 
         self._estimate_accuracy(ys, self.ys_true)
         # self.regularizationLoss = torch.max(self.loss_ys(ys, self.ys_uncertain),
         #                                    self.loss_yu(yu, self.ys_uncertain))
         # self._alpha = 0.5 - (0.5 - self._last_epoch_accuracy)
-        self.regularizationLoss = (self.loss_ys(ys, self.yu_true) +
-                                   self.loss_yu(yu, self.ys_true)) / 2
+        rLoss = (self.loss_ys(ys, self.ys_uncertain))# +
+                                  # self.loss_yu(yu, self.ys_uncertain)) / 2
         # return the output on the supervised sample:
         supervised_loss = loss
-        loss = supervised_loss * (1 - self._alpha) + self._alpha * self.regularizationLoss
+        loss = supervised_loss * (1 - self._alpha) + self._alpha * rLoss
         # print("loss: {0:.2f} supervised: {1:.2f} unsupervised: {2:.2f} ".format(loss.data[0], supervised_loss.data[0], self.regularizationLoss.data[0]))
-        return (loss, supervised_loss.data[0], self.regularizationLoss.data[0])
+        return (loss, supervised_loss.data[0], rLoss.data[0])
+
+    def get_which_one_model(self):
+        return self._which_one_model
 
     def resume(self, saved_model):
         self.checkpoint_model = saved_model

src/org/campagnelab/dl/pytorch/ureg/tests/URegTest.py
@@ -2,7 +2,7 @@ import unittest
 
 import torch
 from torch.autograd import Variable
-from torch.nn import Module, BCELoss
+from torch.nn import Module, BCELoss, Softmax
 
 from org.campagnelab.dl.pytorch.ureg.URegularizer import URegularizer
 from org.campagnelab.dl.pytorch.ureg.tests.SimpleModel import SimpleModel
@@ -12,7 +12,7 @@ class URegTest(unittest.TestCase):
     def setUp(self):
         self.model = SimpleModel(2, 1)
         self.criterion = BCELoss()
-        self.x = Variable(torch.cat([torch.rand(1), torch.ones(1)]))
+        self.x = Variable(torch.cat([torch.rand(1), torch.ones(1)]),requires_grad=True)
         self.y = self.model.forward(self.x)
         self.y_true = Variable(torch.ones(1), requires_grad=False)
         self.epsilon = 1e-6
@@ -39,7 +39,7 @@ class URegTest(unittest.TestCase):
         """
         ureg = URegularizer(self.model, 1, num_features=2,
                             alpha=1,  # gradient only from ureg.
-                            learning_rate=1)
+                            learning_rate=0.1)
 
         loss = self.criterion(self.y, self.y_true)
         xs = self.x
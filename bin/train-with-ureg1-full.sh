5c0e912948b2558c591ab6c377ee0630bf37b225
Fabien Campagne <fac2003@campagnelab.org>
2017-11-21 13:09:26 -0500

    Add ureg1 script to train with full cifar10 training set.

---
+++ bin/train-with-ureg1-full.sh
@@ -0,0 +1,6 @@
+# Run evaluate with a main model learning rate set to 0.1 and ureg alpha=0.1.
+
+python src/org/campagnelab/dl/pytorch/cifar10/Evaluate.py \
+-n 50000 --mini-batch-size 128 --ureg --checkpoint-key DEBUG_UREG1_FULL \
+--lr 0.01 --ureg-learning-rate 0.01 --ureg-alpha 0.5 --ureg-num-features 640 \
+--model VGG16

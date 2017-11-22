cc7dc054f5b6ac8569c9d3d0036cc69777bf5258
Fabien Campagne <fac2003@campagnelab.org>
2017-11-21 23:56:18 -0500

    Fix checkpoint key.

--- bin/train-with-ureg2-full.sh
+++ bin/train-with-ureg2-full.sh
@@ -2,6 +2,6 @@
 
 
 python src/org/campagnelab/dl/pytorch/cifar10/Evaluate2.py \
--n 50000 --mini-batch-size 128 --ureg --checkpoint-key DEBUG_UREG1_FULL \
+-n 50000 --mini-batch-size 128 --ureg --checkpoint-key UREG2_FULL \
 --lr 0.01 --ureg-learning-rate 0.0001 --shave-lr 0.001 --ureg-alpha 1 --ureg-num-features 640 \
 --model VGG16 --ureg-reset-every-n-epoch  50 --shaving-epochs 1 --num-epochs 2000

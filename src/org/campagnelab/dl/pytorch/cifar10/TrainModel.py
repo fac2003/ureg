0e66bbe90dfbc5288a32349773e753c58f4c89bd
Fabien Campagne <fac2003@campagnelab.org>
2017-11-22 18:23:08 -0500

    Fix adjustment when parameters are not specified.

--- src/org/campagnelab/dl/pytorch/cifar10/TrainModel.py
+++ src/org/campagnelab/dl/pytorch/cifar10/TrainModel.py
@@ -72,9 +72,16 @@ class TrainModel:
         args = self.args
         mini_batch_size = self.mini_batch_size
         # restrict limits to actual size of datasets:
-        args.num_training=min(len(self.problem.train_loader())*mini_batch_size,args.num_training)
-        args.num_shaving=min(len(self.problem.reg_loader())*mini_batch_size,args.num_shaving)
-        args.num_validation=min(len(self.problem.test_loader())*mini_batch_size,args.num_validation)
+        training_set_length = len(self.problem.train_loader())*mini_batch_size
+        if args.num_training> training_set_length:
+            args.num_training= training_set_length
+        unsup_set_length = len(self.problem.reg_loader())*mini_batch_size
+        if args.num_shaving > unsup_set_length:
+            args.num_shaving = unsup_set_length
+        test_set_length = len(self.problem.test_loader())*mini_batch_size
+        if args.num_validation > test_set_length:
+            args.num_validation = test_set_length
+
         self.unsuploader = self.problem.reg_loader()
 
         if args.resume:

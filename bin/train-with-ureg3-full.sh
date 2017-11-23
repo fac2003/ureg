472799d6fed3de9bf610fd0c79af6f8dd54c5d5d
Fabien Campagne <fac2003@campagnelab.org>
2017-11-23 16:18:46 -0500

    Put scripts on different GPUs.

--- bin/train-with-ureg3-full.sh
+++ bin/train-with-ureg3-full.sh
@@ -1,5 +1,5 @@
 # Run evaluate3
-
+export CUDA_VISIBLE_DEVICES=3
 python src/org/campagnelab/dl/pytorch/cifar10/Evaluate3.py \
 -n 50000 -x 10000 -u 10000 --mini-batch-size 128 --ureg --checkpoint-key UREG3_FULL \
 --lr 0.01 --ureg-learning-rate 0.001 --shave-lr 0.001 --ureg-alpha 0.5 \
--- bin/train-with-ureg3-stl10.sh
+++ bin/train-with-ureg3-stl10.sh
@@ -1,5 +1,5 @@
 # Run evaluate3
-
+export CUDA_VISIBLE_DEVICES=4
 python src/org/campagnelab/dl/pytorch/cifar10/Evaluate3.py \
 -n 5000 -x 8000 -u 100000 --mini-batch-size 128 --ureg --checkpoint-key STL10_UREG3_FULL \
 --lr 0.1 --ureg-learning-rate 0.1 --shave-lr 0.1 --ureg-alpha 0.5 \

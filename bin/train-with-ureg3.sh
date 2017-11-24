9550090c75cb28915db1843ed6409c5914f8676f
Fabien Campagne <fac2003@campagnelab.org>
2017-11-24 13:56:19 -0500

    Set more aggressive shaving parameters.

--- bin/train-with-ureg3.sh
+++ bin/train-with-ureg3.sh
@@ -1,7 +1,7 @@
 # Run evaluate3
 
 python src/org/campagnelab/dl/pytorch/cifar10/Evaluate3.py \
--n 1000 -x 10000 -u 10000 --mini-batch-size 128 --ureg --checkpoint-key DEBUG_UREG3_1000 \
---lr 0.01 --ureg-learning-rate 0.001 --shave-lr 0.01 --ureg-alpha 0.5 \
---ureg-num-features 640 --num-epochs 10000 \
---model VGG16 --shaving-epochs 1 --ureg-reset-every-n-epoch 50
+-n 1000 -x 10000 -u 10000 --mini-batch-size 128 --ureg --checkpoint-key DEBUG_UREG3_1000_se=5 \
+--lr 0.001 --ureg-learning-rate 0.001 --shave-lr 0.01 --ureg-alpha 0.5 \
+--ureg-num-features 640 --num-epochs 10000 --shaving-epochs 2 \
+--model VGG16 --ureg-reset-every-n-epoch 50

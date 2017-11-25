56d209ad0c9e92eb8994be2d7d7f9957f0d86bdd
Fabien Campagne <fac2003@campagnelab.org>
2017-11-25 13:02:28 -0500

    Update parameters for supervised model training (for 96x96 pixel dimension)

--- bin/train-with-ureg3-stl10.sh
+++ bin/train-with-ureg3-stl10.sh
@@ -1,7 +1,8 @@
 # Run evaluate3
 export CUDA_VISIBLE_DEVICES=4
 python src/org/campagnelab/dl/pytorch/cifar10/Evaluate3.py \
--n 5000 -x 8000 -u 10000 --mini-batch-size 128 --ureg --checkpoint-key STL10_UREG3_10K-mode-combined \
+-n 5000 -x 8000 -u 10000 --mini-batch-size 64 --ureg --checkpoint-key STL10_UREG3_10K-mode-combined-96 \
 --lr 0.01 --ureg-learning-rate 0.0001 --shave-lr 0.0001 --ureg-alpha 0.5 \
 --ureg-num-features 522 --num-epochs 10000 --problem STL10 --mode combined \
---model VGG16 --shaving-epochs 1 --ureg-reset-every-n-epoch 20 --max-examples-per-epoch 10000
+--model VGG16 --shaving-epochs 1 --ureg-reset-every-n-epoch 10 \
+--max-examples-per-epoch 10000

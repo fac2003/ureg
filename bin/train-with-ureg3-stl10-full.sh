f9818ec7fbec177dab88487bc5ace29f3752cbcb
Fabien Campagne <fac2003@campagnelab.org>
2017-11-25 00:31:48 -0500

    Add mode combined for stl10-full.

--- bin/train-with-ureg3-stl10-full.sh
+++ bin/train-with-ureg3-stl10-full.sh
@@ -1,8 +1,6 @@
 # Run evaluate3
 export CUDA_VISIBLE_DEVICES=4
 python src/org/campagnelab/dl/pytorch/cifar10/Evaluate3.py \
--n 5000 -x 8000 -u 100000 --mini-batch-size 128 --ureg --checkpoint-key STL10_UREG3_100K \
---lr 0.1 --ureg-learning-rate 0.1 --shave-lr 0.1 --ureg-alpha 0.5 \
---ureg-num-features 522 --num-epochs 10000 --problem STL10 \
---model VGG16 --shaving-epochs 1 --ureg-reset-every-n-epoch 50 \
---max-examples-per-epoch 10000
+-n 5000 -x 8000 -u 100000 --mini-batch-size 128 --ureg --checkpoint-key STL10_UREG3_100K-mode-combined \
+--lr 0.01 --ureg-learning-rate 0.0001 --shave-lr 0.0001 --ureg-alpha 0.5 \
+--ureg-num-features 522 --num-epochs 10000 --problem STL10 --mode combined \
--- bin/train-with-ureg3-stl10.sh
+++ bin/train-with-ureg3-stl10.sh
@@ -1,7 +1,7 @@
 # Run evaluate3
 export CUDA_VISIBLE_DEVICES=4
 python src/org/campagnelab/dl/pytorch/cifar10/Evaluate3.py \
--n 5000 -x 8000 -u 10000 --mini-batch-size 128 --ureg --checkpoint-key STL10_UREG3_10K \
---lr 0.001 --ureg-learning-rate 0.0001 --shave-lr 0.0001 --ureg-alpha 0.5 \
---ureg-num-features 522 --num-epochs 10000 --problem STL10 \
+-n 5000 -x 8000 -u 10000 --mini-batch-size 128 --ureg --checkpoint-key STL10_UREG3_10K-mode-combined \
+--lr 0.01 --ureg-learning-rate 0.0001 --shave-lr 0.0001 --ureg-alpha 0.5 \
+--ureg-num-features 522 --num-epochs 10000 --problem STL10 --mode combined \
 --model VGG16 --shaving-epochs 1 --ureg-reset-every-n-epoch 20 --max-examples-per-epoch 10000

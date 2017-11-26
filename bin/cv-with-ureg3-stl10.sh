07cec4eb0c4285beaae33ad9b59b9c37501b5d49
Fabien Campagne <fac2003@campagnelab.org>
2017-11-26 15:32:50 -0500

    Tweak number of samples given the cross-validation setting.

--- bin/cv-with-ureg3-stl10.sh
+++ bin/cv-with-ureg3-stl10.sh
@@ -1,7 +1,8 @@
 # Run evaluate3
 python ${UREG}/src/org/campagnelab/dl/pytorch/cifar10/Evaluate3.py \
--n 5000 -x 8000 -u 100000 --mini-batch-size 64 --ureg \
+-n 1000 -x 4000 -u 100000 --mini-batch-size 64 --ureg \
 --checkpoint-key STL10_UREG3_100K-mode-combined-VGG16_${RANDOM} \
 --problem STL10 --mode combined \
---model VGG16 --shaving-epochs 1 --cross-validations-folds ${UREG}/data/stl10_binary/fold_indices.txt \
+--model VGG16 --shaving-epochs 1 \
+ --cross-validations-folds ${UREG}/data/stl10_binary/fold_indices.txt \
 --max-examples-per-epoch 10000 --constant-learning-rates "$@"

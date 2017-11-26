ddab1de3ea718a3ebdb1a398447691e3bd278567
Fabien Campagne <fac2003@campagnelab.org>
2017-11-25 23:54:21 -0500

    Running on server overnight.

--- bin/train-with-ureg3-full.sh
+++ bin/train-with-ureg3-full.sh
@@ -1,7 +1,7 @@
 # Run evaluate3
 python ${UREG}/src/org/campagnelab/dl/pytorch/cifar10/Evaluate3.py \
--n 5000 -x 8000 -u 100000 --mini-batch-size 128 --ureg \
---checkpoint-key STL10_UREG3_100K-mode-combined-VGG19_${RANDOM} \
+-n 5000 -x 8000 -u 100000 --mini-batch-size 64 --ureg \
+--checkpoint-key STL10_UREG3_100K-mode-combined-VGG16_${RANDOM} \
 --problem STL10 --mode combined \
---model VGG19 --shaving-epochs 1 \
+--model VGG16 --shaving-epochs 1 \
 --max-examples-per-epoch 10000 --constant-learning-rates "$@"

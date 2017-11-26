aad2b938c2af1b16094bb8ecac53df0199470ed1
Fabien Campagne <fac2003@campagnelab.org>
2017-11-25 23:41:36 -0500

    propagate arguments.

--- bin/train-with-ureg3-full.sh
+++ bin/train-with-ureg3-full.sh
@@ -1,7 +1,7 @@
 # Run evaluate3
-python src/org/campagnelab/dl/pytorch/cifar10/Evaluate3.py \
--n 5000 -x 8000 -u 100000 --mini-batch-size 256 --ureg \
+python ${UREG}/src/org/campagnelab/dl/pytorch/cifar10/Evaluate3.py \
+-n 5000 -x 8000 -u 100000 --mini-batch-size 128 --ureg \
 --checkpoint-key STL10_UREG3_100K-mode-combined-VGG19_${RANDOM} \
 --problem STL10 --mode combined \
 --model VGG19 --shaving-epochs 1 \
---max-examples-per-epoch 10000 --constant-learning-rates $*
+--max-examples-per-epoch 10000 --constant-learning-rates "$@"

e55a5006d1a42dad820a43fc0511e2363bb6b726
Fabien Campagne <fac2003@campagnelab.org>
2017-11-27 09:59:26 -0500

    Make num_workers configurable, default to zero (may help with crashes when running several Evaluate3 in parallel during CV hyper param search).

--- src/org/campagnelab/dl/pytorch/cifar10/STL10Problem.py
+++ src/org/campagnelab/dl/pytorch/cifar10/STL10Problem.py
@@ -52,8 +52,9 @@ class STL10Problem(Problem):
     def example_size(self):
         return (3, 96, 96)
 
-    def __init__(self, mini_batch_size):
+    def __init__(self, mini_batch_size, num_workers=0):
         super().__init__(mini_batch_size)
+        self.num_workers=num_workers
         from PIL import Image
         self.transform_train = transforms.Compose([
             transforms.RandomCrop(96, padding=4),
@@ -81,7 +82,7 @@ class STL10Problem(Problem):
 
         trainloader = torch.utils.data.DataLoader(self.trainset, batch_size=mini_batch_size, shuffle=False,
                                                   collate_fn=stl10_collate,
-                                                  num_workers=2)
+                                                  num_workers=self.num_workers)
         return trainloader
 
     def train_loader_subset(self, indices):
@@ -93,7 +94,7 @@ class STL10Problem(Problem):
                                                   sampler=ProtectedSubsetRandomSampler(
                                                       indices),
                                                   collate_fn=stl10_collate,
-                                                  num_workers=2)
+                                                  num_workers=self.num_workers)
         return trainloader
 
     def test_loader(self):
@@ -101,7 +102,8 @@ class STL10Problem(Problem):
         mini_batch_size = self.mini_batch_size()
         return torch.utils.data.DataLoader(self.testset,
                                            collate_fn=stl10_collate,
-                                           batch_size=mini_batch_size, shuffle=False, num_workers=2)
+                                           batch_size=mini_batch_size, shuffle=False,
+                                           num_workers=self.num_workers)
 
     def test_loader_subset(self, indices):
         """Returns the torch dataloader over the test set, limiting to the examples
@@ -111,14 +113,15 @@ class STL10Problem(Problem):
         return torch.utils.data.DataLoader(self.testset,
                                            collate_fn=stl10_collate,
                                            sampler=ProtectedSubsetRandomSampler(indices),
-                                           batch_size=mini_batch_size, shuffle=False, num_workers=2)
+                                           batch_size=mini_batch_size, shuffle=False,
+                                           num_workers=self.num_workers)
 
     def reg_loader(self):
         mini_batch_size = self.mini_batch_size()
 
         return torch.utils.data.DataLoader(self.unsupset, batch_size=mini_batch_size, shuffle=True,
                                            collate_fn=stl10_collate,
-                                           num_workers=2)
+                                           num_workers=self.num_workers)
 
     def reg_loader_subset(self, indices):
         """Returns the torch dataloader over the regularization set (unsupervised examples only). """
@@ -128,7 +131,7 @@ class STL10Problem(Problem):
         return torch.utils.data.DataLoader(self.unsupset, batch_size=mini_batch_size, shuffle=False,
                                            sampler=ProtectedSubsetRandomSampler(indices),
                                            collate_fn=stl10_collate,
-                                           num_workers=2)
+                                           num_workers=self.num_workers)
 
     def loss_function(self):
         return torch.nn.CrossEntropyLoss()

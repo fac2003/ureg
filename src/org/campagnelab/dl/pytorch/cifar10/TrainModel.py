b9ab05460c76aa3f364c7de5b69d86404a1d4df4
Fabien Campagne <fac2003@campagnelab.org>
2017-11-28 09:19:07 -0500

    When learning rates are constant and --ureg-reset-every-n-epoch is specified, reset the ureg model periodically.

--- src/org/campagnelab/dl/pytorch/cifar10/TrainModel.py
+++ src/org/campagnelab/dl/pytorch/cifar10/TrainModel.py
@@ -539,8 +539,9 @@ class TrainModel:
 
             self.grow_unsupervised_examples_per_epoch()
             to_reset_ureg_model += 1
-            if self.args.constant_learning_rates and to_reset_ureg_model > self.args.reset_every_n_epoch:
-                # when learning rates are constant and reset_every_n_epoch is specified, reset the ureg model periodically:
+            if self.args.constant_learning_rates and to_reset_ureg_model > self.args.ureg_reset_every_n_epoch:
+                # when learning rates are constant and ureg_reset_every_n_epoch is specified,
+                # reset the ureg model periodically:
                 self.ureg._which_one_model = None
                 to_reset_ureg_model = 0
 

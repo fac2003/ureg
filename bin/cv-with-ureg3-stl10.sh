c381524fc7d11b137c474b1b203e11ac63fd894e
Fabien Campagne <fac2003@campagnelab.org>
2017-11-28 13:41:55 -0500

    Reset ureg model every 10 epochs.

--- bin/cv-with-ureg3-stl10.sh
+++ bin/cv-with-ureg3-stl10.sh
@@ -5,5 +5,6 @@ python ${UREG}/src/org/campagnelab/dl/pytorch/cifar10/Evaluate3.py \
 --checkpoint-key ${CHECKPOINT} \
 --problem STL10  \
 --model VGG16 --shaving-epochs 1 \
+--ureg-reset-every-n-epoch 10  \
  --cross-validations-folds ${UREG}/data/stl10_binary/fold_indices.txt \
 --max-examples-per-epoch 1000 --constant-learning-rates "$@" 2>&1 |tee ${CHECKPOINT}.log

50c17f10106c7cc8d25d1618717568463b9cbd4a
Fabien Campagne <fac2003@campagnelab.org>
2017-12-08 15:40:33 -0500

    Update parameters.

--- bin/cv-with-split1-stl10.sh
+++ bin/cv-with-split1-stl10.sh
@@ -1,10 +1,11 @@
-# Run evaluate3
+#!/usr/bin/env bash
+# Run evaluate4
 CHECKPOINT=STL10_${RANDOM}
 python ${UREG}/src/org/campagnelab/dl/pytorch/cifar10/Evaluate4.py \
--n 5000 -x 8000 -u 100000 --mini-batch-size 50 --ureg \
+-n 5000 -x 8000 -u 100000 --mini-batch-size 100  \
 --checkpoint-key ${CHECKPOINT} \
 --problem STL10  \
---model VGG16 --shaving-epochs 1 \
+--model VGG16  \
 --cv-fold-min-perf 0.57 \
 --cross-validations-folds ${UREG}/data/stl10_binary/fold_indices.txt \
   "$@" 2>&1 |tee ${CHECKPOINT}.log

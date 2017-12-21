3a84fdc5293c98d2f743e0db97d87c6dd2018c44
Fabien Campagne <fac2003@campagnelab.org>
2017-12-21 16:40:12 -0500

    Adjust means and standard deviations of channels for STL10.

--- src/org/campagnelab/dl/pytorch/cifar10/STL10Problem.py
+++ src/org/campagnelab/dl/pytorch/cifar10/STL10Problem.py
@@ -71,14 +71,16 @@ class STL10Problem(Problem):
             transforms.RandomHorizontalFlip(),
             #transforms.Grayscale(num_output_channels=3),
             transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225]),
 
         ])
 
         self.transform_test = transforms.Compose([
             #transforms.Grayscale(num_output_channels=3),
             transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225]),
         ])
         self._trainset = torchvision.datasets.STL10(root='./data', split="train", download=True,
                                                     transform=self.transform_train)

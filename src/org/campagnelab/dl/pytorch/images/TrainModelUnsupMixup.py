Fix draft of fm_loss normalization. Inefficient at this time because loss is calculated on the cpu.

src/org/campagnelab/dl/pytorch/images/TrainModelUnsupMixup.py
@@ -14,7 +14,7 @@ from org.campagnelab.dl.pytorch.images.FloatHelper import FloatHelper
 from org.campagnelab.dl.pytorch.images.LRHelper import LearningRateHelper
 from org.campagnelab.dl.pytorch.images.LossHelper import LossHelper
 from org.campagnelab.dl.pytorch.images.PerformanceList import PerformanceList
-from org.campagnelab.dl.pytorch.images.models.dual import LossEstimator
+from org.campagnelab.dl.pytorch.images.models.dual import LossEstimator_L1
 from org.campagnelab.dl.pytorch.images.models.vgg_dual import VGGDual
 from org.campagnelab.dl.pytorch.images.utils import progress_bar, grad_norm, init_params
 from org.campagnelab.dl.pytorch.ureg.LRSchedules import construct_scheduler
@@ -305,15 +305,14 @@ class TrainModelUnsupMixup:
         return performance_estimators
 
     def train_with_fm_loss(self, epoch,
-                    performance_estimators=None
-                    ):
+                    gamma=1E-5, performance_estimators=None):
 
         if performance_estimators is None:
             performance_estimators = PerformanceList()
             performance_estimators += [LossHelper("optimized_loss")]
             performance_estimators += [LossHelper("train_loss")]
             performance_estimators += [FloatHelper("fm_loss")]
-            # performance_estimators += [AccuracyHelper("train_")]
+            performance_estimators += [AccuracyHelper("train_")]
             performance_estimators += [FloatHelper("train_grad_norm")]
 
             print('\nTraining, epoch: %d' % epoch)
@@ -346,22 +345,26 @@ class TrainModelUnsupMixup:
             self.net.train()
             self.net.zero_grad()
             self.optimizer_training.zero_grad()
+            inputs, targets, uinputs = Variable(inputs), Variable(targets, requires_grad=False), Variable(uinputs, requires_grad=True)
+            if self.use_cuda:
+                inputs, targets, uinputs=inputs.cuda(),targets.cuda(), uinputs.cuda()
             outputs, outputu, fm_loss = self.net(inputs,uinputs)
 
-            if train_supervised_model:
-                supervised_loss = self.criterion(outputs, targets)
-                optimized_loss = supervised_loss+fm_loss
-                optimized_loss.backward()
-                self.optimizer_training.step()
-                supervised_grad_norm = grad_norm(self.net.parameters())
-                performance_estimators.set_metric(batch_idx, "train_grad_norm", supervised_grad_norm)
-                performance_estimators.set_metric(batch_idx, "optimized_loss", optimized_loss.data[0])
-                performance_estimators.set_metric(batch_idx, "fm_loss", fm_loss.data[0])
+            supervised_loss = self.criterion(outputs, targets)
+            optimized_loss = supervised_loss+gamma*fm_loss
+            optimized_loss.backward()
+            self.optimizer_training.step()
+            supervised_grad_norm = grad_norm(self.net.parameters())
+            performance_estimators.set_metric(batch_idx, "train_grad_norm", supervised_grad_norm)
+            performance_estimators.set_metric(batch_idx, "optimized_loss", optimized_loss.data[0])
+            performance_estimators.set_metric(batch_idx, "fm_loss", fm_loss)
 
-                performance_estimators.set_metric_with_outputs(batch_idx, "train_loss", supervised_loss.data[0],
-                                                               outputs, targets)
-                # performance_estimators.set_metric_with_outputs(batch_idx, "train_accuracy", supervised_loss.data[0],
-                #                                               outputs, targets)
+            performance_estimators.set_metric_with_outputs(batch_idx, "train_loss", supervised_loss.data[0],
+                                                           outputs, targets)
+            performance_estimators.set_metric_with_outputs(batch_idx, "train_accuracy", supervised_loss.data[0],
+                                                           outputs, targets)
+            # performance_estimators.set_metric_with_outputs(batch_idx, "train_accuracy", supervised_loss.data[0],
+            #                                               outputs, targets)
 
             progress_bar(batch_idx * self.mini_batch_size,
                          min(self.max_regularization_examples, self.max_training_examples),
@@ -493,7 +496,10 @@ class TrainModelUnsupMixup:
             if self.use_cuda:
                 inputs, targets = inputs.cuda(), targets.cuda()
             inputs, targets = Variable(inputs, volatile=True), Variable(targets, volatile=True)
-            outputs = self.net(inputs)
+            if hasattr(self.net, 'dual'):
+                outputs = self.net(inputs)
+            else:
+                outputs, _, _ =self.net(inputs,None)
             loss = self.criterion(outputs, targets)
             # accumulate the confusion matrix:
             _, predicted = torch.max(outputs.data, 1)
@@ -669,9 +675,14 @@ class TrainModelUnsupMixup:
         :return list of performance estimators that observed performance on the last epoch run.
         """
         header_written = False
-        self.net=VGGDual(vgg_name="VGG16", input_shape=self.problem.example_size(), loss_estimator=LossEstimator())
+        self.net=VGGDual(vgg_name="VGG16", input_shape=self.problem.example_size(), loss_estimator=LossEstimator_L1,                         )
+        if self.use_cuda:
+            self.net.cuda()
         # TODO: determine if init_params work with dual:
-        init_params(self.net)
+        #init_params(self.net)
+        self.net.apply(init_params)
+        self.optimizer_training = torch.optim.SGD(self.net.parameters(), lr=self.args.lr, momentum=self.args.momentum,
+                                                  weight_decay=self.args.L2)
 
         lr_train_helper = LearningRateHelper(scheduler=self.scheduler_train, learning_rate_name="train_lr")
         previous_test_perfs = None
@@ -681,7 +692,7 @@ class TrainModelUnsupMixup:
         for epoch in range(self.start_epoch, self.start_epoch + self.args.num_epochs):
 
             perfs = PerformanceList()
-            perfs += self.train_with_fm_loss(epoch)
+            perfs += self.train_with_fm_loss(epoch, self.args.gamma)
 
             perfs += [lr_train_helper]
             if previous_test_perfs is None or self.epoch_is_test_epoch(epoch):

src/org/campagnelab/dl/pytorch/images/models/dual.py
@@ -4,34 +4,57 @@ import torch
 from torch.autograd import Variable
 from torch.nn import Module
 
-class LossEstimator:
-    def loss(self, xs, xu):
-        xs=xs.detach()
-        xu=xu.detach()
-        return torch.norm(xs-xu,p=1)
+def LossEstimator_L1( xs, xu):
+        #xs=xs.detach()
+        #xu=xu.detach()
+        loss=0
+        bs=xs.size()[0]
+        xs=xs.cpu()
+        xu=xu.cpu()
+        abs = torch.abs(xs - xu)
+
+        n=0
+        for index_s in range(xs.size()[0]):
+            for index_u in range(xu.size()[0]):
+                if index_s<=index_u:
+                    norm =abs[index_s].norm(p=1)
+                    #squared_norm = norm * norm
+                    # regularize so that the features are either similar or orthogonal:
+                    dot_product = xs[index_s].dot(xu[index_u])
+                    loss=loss+torch.min(norm, dot_product).data[0]
+                    n+=1
+        return loss/n
 
 class Dual(Module):
     def __init__(self, loss_estimator=None, delegate_module=None):
-        super(Dual).__init__()
+        super().__init__()
         assert loss_estimator is not None
         assert delegate_module is not None
         self.delegate_module=delegate_module
         self.loss_estimator=loss_estimator
 
+
     def forward(self, xs, xu):
+        loss_weight=0.0
         for index, module in enumerate(self._modules.values()):
             xs = self.delegate_module(xs)
-            xu = self.delegate_module(xs)
-            self.loss_weight[index]*self.loss_estimator(xs,xu)
-        return (xs,xu, self.loss_weight)
+            if xu is not None:
+                xu = self.delegate_module(xu)
+                loss_weight=self.loss_estimator(xs,xu)
+        return (xs,xu, loss_weight)
 
+    def cuda(self, device=None):
+        self.delegate_module.cuda(device)
 
 class SequentialDual(Module):
     """A dual sequential container. Makes it easy to calculate agreement loss between feature maps.
-
     """
 
-    def __init__(self, loss_estimator, args):
+    def __init__(self, loss_estimator,  args):
+        """
+        :param loss_estimator: a function f(xs,xu) that returns the loss for agreement between xs and xu.
+        :param args:
+        """
         super(SequentialDual, self).__init__()
         if len(args) == 1 and isinstance(args[0], OrderedDict):
             for key, module in args[0].items():
@@ -39,7 +62,7 @@ class SequentialDual(Module):
         else:
             for idx, module in enumerate(args):
                 self.add_module(str(idx), module)
-        self.loss_weight=Variable(torch.ones(len(self)),requires_grad=True)
+        self.loss_weight=Variable(torch.zeros(1),requires_grad=True)
         self.loss_estimator=loss_estimator
 
     def __getitem__(self, idx):
@@ -56,10 +79,13 @@ class SequentialDual(Module):
         return len(self._modules)
 
     def forward(self, xs, xu):
-        loss_weight=self.loss_weight.clone()
+        loss_weight=0.0
+
+
         for index, module in enumerate(self._modules.values()):
             xs = module(xs)
-            xu = module(xu)
-            loss_weight[index]=self.loss_estimator.loss(xs,xu)
-        return (xs,xu, torch.sum(loss_weight))
+            if xu is not None:
+                xu = module(xu)
+                loss_weight+=self.loss_estimator(xs,xu)
+        return (xs,xu, loss_weight)
 

src/org/campagnelab/dl/pytorch/images/models/feature_size_calculator.py
@@ -17,12 +17,11 @@ class EstimateFeatureSize(Module):
         n_size = output_feat.data.view(bs, -1).size(1)
         return n_size
 
-    def estimate_output_size_with_dual_model(self, input_shape, model, use_cuda=False):
+    def estimate_output_size_with_dual_model(self, input_shape, model):
         """Calculate the convolution output size using a partially constructed model. """
         bs = 1
         input = Variable(torch.rand(bs, *input_shape))
-        if use_cuda:
-            input=input.cuda()
+
         output_feat,_, _ = model(input,input)
         n_size = output_feat.data.view(bs, -1).size(1)
         return n_size

src/org/campagnelab/dl/pytorch/images/models/vgg_dual.py
@@ -18,18 +18,23 @@ cfg = {
 class VGGDual(EstimateFeatureSize):
 
     """A dual version of the VGG models. """
-    def __init__(self, vgg_name, input_shape=None,loss_estimator=None):
-        super(VGGDual, self).__init__()
+    def __init__(self, vgg_name, input_shape=None,loss_estimator=None, use_cuda=False):
+        super().__init__()
         assert input_shape is not None, "You must provide the image input shape."
         assert  loss_estimator is not None
         self.loss_estimator=loss_estimator
-        self.features = self._make_layers(cfg[vgg_name],loss_estimator)
+
+        self.features = self._make_layers(cfg[vgg_name],loss_estimator, use_cuda)
         self.num_out = self.estimate_output_size_with_dual_model(input_shape, model=self.features)
-        self.remake_classifier(10, False)
+        self.remake_classifier(10, use_cuda)
+        self.use_cuda = use_cuda
+
+
 
     def remake_classifier(self, num_classes, use_cuda, dropout_p=0.5):
         self.classifier = Dual(delegate_module=nn.Linear(self.num_out, num_classes), loss_estimator=self.loss_estimator)
-        if use_cuda: self.classifier = self.classifier.cuda()
+        if use_cuda:
+            self.classifier.cuda()
 
     def get_classifier(self):
         return self.classifier
@@ -38,12 +43,15 @@ class VGGDual(EstimateFeatureSize):
 
         outs,outu,features_fm_loss = self.features(xs,xu)
         outs = outs.view(outs.size(0), -1)
-        outu = outu.view(outu.size(0), -1)
+        if outu is not None:
+            outu = outu.view(outu.size(0), -1)
         outs, outu, classifier_fm_loss = self.classifier(outs, outu)
         return (outs, outu, features_fm_loss+classifier_fm_loss)
 
-
-    def _make_layers(self, cfg, loss_estimator):
+    def apply(self, fn):
+        self.features.apply(fn)
+        self.classifier.apply(fn)
+    def _make_layers(self, cfg, loss_estimator, use_cuda):
         layers = []
         in_channels = 3
         for x in cfg:
@@ -52,10 +60,10 @@ class VGGDual(EstimateFeatureSize):
             else:
                 layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
                            nn.BatchNorm2d(x),
-                           nn.ReLU(inplace=True)]
+                           nn.ReLU(inplace=False)]
                 in_channels = x
         layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
-        return SequentialDual(loss_estimator=loss_estimator,args=layers)
+        return SequentialDual(loss_estimator=loss_estimator, args=layers)
 
 # net = VGG('VGG11')
 # x = torch.randn(2,3,32,32)